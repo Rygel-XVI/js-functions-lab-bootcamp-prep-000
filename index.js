0078243c7d94a8d69482d6c5abd8ec3e572923b6
Annette Michaels <micha232@umn.edu>
2017-09-29 18:25:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ function happyHolidays() {
 }
 
 function happyHolidaysTo(name) {
-  return "Happy holidays, " + name +"!"
+<<<<<<< HEAD
+  return 'Happy holidays, ${name}'
 }
 
 function happyHolidayTo(holiday, name) {
@@ -13,4 +14,15 @@ function happyHolidayTo(holiday, name) {
 
 function holidayCountdown(holiday, days) {
   return "It's " + days + " days until " + holiday +"!"
+=======
+  console.log("Happy holidays, ${name}!")
+  return "Happy holidays, ${name}!"
+}
+function happyHolidayTo(holiday, name) {
+  return "Happy ${holiday}, ${name}!"
+}
+
+function holidayCountdown(holiday, days) {
+  return "It's ${days} days until ${holiday}!"
+>>>>>>> 85cf45e148e420035370385a6bc8f3592d9e8d07
 }
